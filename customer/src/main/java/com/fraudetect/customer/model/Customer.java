376d172a5bcd3b95930f0403907982ee4bbe2628
hedaya Mostafa <hedayaselim1807@gmail.com>
2023-10-23 16:50:28 +0200

    feat(customer): config the database and create customer repository and start adding new customers to the database

--- customer/src/main/java/com/fraudetect/customer/model/Customer.java
+++ customer/src/main/java/com/fraudetect/customer/model/Customer.java
@@ -1,13 +1,29 @@
 package com.fraudetect.customer.model;
 
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Customer {
-    private Integer id;
+
+    @Id
+    @SequenceGenerator(name = "customer_id_sequence",
+    sequenceName = "customer_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+    generator = "customer_id_sequence")
+    private Long id;
     private String firstName;
     private String lastName;
     private String email;
--- customer/src/main/java/com/fraudetect/customer/service/CustomerService.java
+++ customer/src/main/java/com/fraudetect/customer/service/CustomerService.java
@@ -2,10 +2,11 @@ package com.fraudetect.customer.service;
 
 import com.fraudetect.customer.model.Customer;
 import com.fraudetect.customer.model.CustomerRegistrationRequest;
+import com.fraudetect.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository repository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -16,5 +17,6 @@ public record CustomerService() {
         // todo: check email is valid
         // todo: check if email is not taken
         // todo: add customer to the db
+        repository.save(customer);
     }
 }
